fe621bd7296183c854be5432922f1edb715a40ca
YJ-Cha <107171334+YJ-Cha@users.noreply.github.com>
2022-07-15 12:23:10 +0900

    0715 modified

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 new fullpage('#fullpage', {
-	//options here
+    //options here
     menu: '.gnb_list',
     anchors: ['s1', 's2', 's3', 's4', 's5'],
     // sectionsColor: ['indianred', 'lemonchiffon', 'mediumaquamarine', 'mediumpurple'],
-	autoScrolling: true,
-	scrollHorizontally: true,
+    autoScrolling: true,
+    scrollHorizontally: true,
     scrollBar: true,
     easing: 'easeInOutCubic',
     navigation: true,
-	navigationPosition: 'right',
-	navigationTooltips: ['GAH소개', '제다큐어', '연구개발', '본사위치', '연구소위치'],
+    navigationPosition: 'right',
+    navigationTooltips: ['GAH소개', '제다큐어', '연구개발', '본사위치', '연구소위치'],
     normalScrollElements: '.s2',
 });
 
+$('.btn_total').click(function(){
+    $('.total_menu_z').addClass('active');
+});
+$('.btn_totalClose').click(function(){
+    $('.total_menu_z').removeClass('active');
+});
+
+var myScroll = new IScroll('.total_menu', {
+    mouseWheel: true,
+    // scrollbars: true
+});
+
+$('.totalM_item').click(function () {
+    if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+    } else {
+        $('.totalM_item').removeClass("active");
+        $(this).addClass("active");
+    }
+});
